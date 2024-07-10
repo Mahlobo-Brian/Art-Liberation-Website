23e345fc658084c428eec8d8cb66abb56e8a27f6
MahloboBrian <44670747+Mahlobo-Brian@users.noreply.github.com>
2024-07-10 02:49:46 -1200

    Add files via upload

---
+++ Gallery.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/ArtLiberatonWhite.png">
+    <!-- icon-Logo image in the title bar -->
+	<link href="assets/img/ArtLiberatonWhite.png" rel="icon">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Wo Without Men | Gallery</title>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <link href="bootstrap3/css/bootstrap.css" rel="stylesheet" />
+	<link href="assets/css/gsdk.css" rel="stylesheet" />  
+    <link href="assets/css/demo.css" rel="stylesheet" /> 
+    <!-- Font Awesome -->
+    <link href="bootstrap3/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+	<!-- Puts socials inline -->
+	<link href="assets/css/socialMediaStyle.css" rel="stylesheet"> 
+	<!-- Instafeed -->
+	<link rel='stylesheet' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
+	<link rel="stylesheet" href="Instafeed/galleryStyle.css">	
+</head>
+<body>
+	<div id="navbar-full">
+		<div class="container">
+			<nav class="navbar navbar-ct-blue navbar-transparent navbar-fixed-top" role="navigation">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Logo -->
+						<div id="logo" class="pull-left">
+							<a href="http://artliberation.co.za"><img src="assets/img/Art-Liberaton(NoBackRound).png" alt="" title="" /></a>
+						</div>
+					</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-center">
+							<li class="active"><a href="http://artliberation.co.za"><i class="fa fa-home"></i> Home</a></li>
+							<li class="active"><a href="http://artliberation.co.za#about"><i class="fa fa-briefcase" aria-hidden="true"></i> About</a></li>
+							<li class="active"><a href="#Clients"><i class="fa fa-picture-o" aria-hidden="true"></i> Gallery</a></li>
+							<li class="active"><a href="Events.php"><i class="fa fa-calendar" aria-hidden="true"></i> Events</a></li>
+							<li class="active"><a href="http://artliberation.co.za#testimonials"><i class="fa fa-video-camera" aria-hidden="true"></i> Videos</a></li>
+							<li class="active"><a href="http://artliberation.co.za#bottom-widget"><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<br /><br />
+	<center/>
+	<div class="container" id="Clients">
+		<div class="wow bounceInDown" data-wow-delay="0.4s">
+			<div class="section-heading"><h2><strong> Gallery </strong></h2></div>
+		</div>
+	</div>
+	<div class="container">
+		<center/>
+		<div id="instafeed-gallery-feed" class="row no-gutter">
+			<!-- instagram feed loads here -->
+		</div>
+		<div class="post-modal-container"></div>
+		<div class="post-modal-backdrop"></div>
+	</div>
+	
+	<br /><br /><center/>	
+	<p>To view more, click the button below </p>
+	<div align="center" id="regButton">    
+		<a class="regButton" target="_blank" title="view more on instagram" href="https://www.instagram.com/art_liberation5102/">Instagram</a>
+	</div>
+	<br /><br />
+	<!-- footer -->
+	<footer id="footer">
+	
+		<div class="container">
+			<div class="row">	
+				<center/>
+				<div class="wow-shake" data-wow-delay="0.4s">					
+					<div class="page-scroll marginbot-30">
+						<a href="#Clients" id="totop" class="btn btn-circle">
+						<i class="fa fa-angle-double-up animated"></i>
+						</a>
+					</div>
+				</div>
+				<div class="col-md-12 col-lg-12">
+					<div class="credits">
+						<p>&copy; 
+							<script>document.write(new Date().getFullYear())</script>
+							Art Liberation. All Rights Reserved
+						</p>
+					</div>
+				</div>
+				<p>We're on social networks</p>
+				<ul class="company-social">                
+					<li class="social-facebook"><a target="_blank" title="like our page on facebook" href="https://www.facebook.com/Art-Liberation-1960424554000216/"><img 
+					alt="follow us on facebook" src="assets/img/socials/facebook-256 (2).png"  width="45" height="45" border=0></a></li> 		   	
+					<li class="social-instagram"><a target="_blank" title="follow us on instagram" href="https://www.instagram.com/art_liberation5102/"><img 
+					alt="follow us on instagram" src="assets/img/socials/Instagram-256(2).png" id="insta" width="42" height="42" border=0></a></li>					
+					<li class="social-pinterest"><a target="_blank" title="follow us on pinterest" href="https://za.pinterest.com/artliberation/"><img 
+					alt="follow us on pinterest" src="assets/img/socials/Pinterest-256(2).png" width="42" height="42" border=0></a></li>					
+					<li class="social-pinterest"><a target="_blank" title="follow us on twitter" href="https://twitter.com/ArtLiberation1"><img 
+					alt="follow us on twitter" src="assets/img/socials/twitter-256(2).png" width="42" height="42" border=0></a></li> 					
+				</ul>
+			</div>
+		</div>	
+	</footer>
+	<!-- footer -->
+
+	
+</body>
+
+    <script src="jquery/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+
+	<script src="bootstrap3/js/bootstrap.js" type="text/javascript"></script>
+	<script src="assets/js/gsdk-checkbox.js"></script>
+	<script src="assets/js/gsdk-radio.js"></script>
+	<script src="assets/js/gsdk-bootstrapswitch.js"></script>
+	<script src="assets/js/get-shit-done.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+	<!-- instagram feed -->
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js'></script>
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js'></script>
+	<script src="Instafeed/index.js"></script>
+	
+</html>
